edf607e79c22d418edc7da0f67c4cc04be0c7a2c
Lantty623 <105613002+Lantty623@users.noreply.github.com>
2023-11-26 16:59:58 -0500

    Add change (#1)
    Co-authored-by: lanttyc <lanttyc@g.clemson.edu>

--- securefolder/addAlias.py
+++ securefolder/addAlias.py
@@ -1,8 +1,16 @@
 import shutil
+import os
 
 if __name__ == "__main__":
-    shutil.copyfile("/home/msg5/.bashrc", "bashRCbackup.txt")
-    additionToBashRC = open("bashfunctionoverwrites.txt", "r").read()
-    with open("/home/msg5/.bashrc", "a") as f:
+    home_directory = os.path.expanduser("~")
+    bashPath = home_directory + "/.bashrc"
+
+    if os.path.exists("securefolder/bashRCbackup.txt"):
+        shutil.copy("securefolder/bashRCbackup.txt", bashPath)
+    else:
+        shutil.copyfile(bashPath, "securefolder/bashRCbackup.txt")
+
+    additionToBashRC = open("securefolder/bashfunctionoverwrites.txt", "r").read()
+    with open(bashPath, "a") as f:
         f.write(additionToBashRC) # add alias here then replace ./bashrc
         f.close()
--- securefolder/startscript.sh
+++ securefolder/startscript.sh
@@ -1,16 +1,13 @@
 #! /user/bin/bash
 echo "Adding scripts to ~/.bashrc"
-python3 addAlias.py
+python3 $(CURDIR)/securefolder/addAlias.py
 echo "creating a /bin directory in home"
 mkdir ~/bin
 echo "copying hashtest.py and securefolder.py scripts to current folder and renaming"
-cp securefolder.py ./securefolder
-cp hashtest.py ./hashtest
 echo "changing permissiong to 744 for both"
-chmod 744 securefolder
-chmod 744 hashtest
+chmod 744 $(CURDIR)/securefolder/securefolder.py
+chmod 744 $(CURDIR)/securefolder/hashtest.py
 echo "moving securefolder and hashtest to ~/bin"
-mv securefolder ~/bin
-mv hashtest ~/bin
+cp $(CURDIR)/securefolder/securefolder.py ~/bin/securefolder
+cp $(CURDIR)/securefolder/hashtest.py ~/bin/hashtest
 echo "should be good to go!"
-source ~/.bashrc
